449613cda4fec55cdc044ed26077bc5033d82ee6
Nicholas Hallahan <nick@theoutpost.io>
2016-02-26 15:58:15 -0800

    initialize socket.io at the correct time

--- pages/deployment/fetch-hot-export/fetch-hot-export.js
+++ pages/deployment/fetch-hot-export/fetch-hot-export.js
@@ -1,4 +1,7 @@
 $(function () {
+    // init socket.io
+    var socket = io.connect();
+
     $('#hot-export-url-fetch-btn').click(function (evt) {
         var postJson = {};
         postJson.url = $('#hot-export-url-input').val();
@@ -18,12 +21,11 @@ $(function () {
                 $('#instructions-div').hide();
                 $('#supporting-msg-txt').html(data.msg);
 
-                // Init socket.io to listen based off
-                // of uuid of fetch-hot-export operation
-                var socket = io.connect(null, { resource: 'socket.io' });
                 socket.on(data.uuid, function (iomsg) {
                     console.log(iomsg);
-                    $('#console').append(iomsg.output);
+                    if (iomsg.output) {
+                        $('#console').append(iomsg.output);
+                    }
                 });
             });
         evt.preventDefault();
