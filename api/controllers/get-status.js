38a7057fcc022526f63c21644bd8cacc3a9d2470
Nicholas Hallahan <nick@theoutpost.io>
2016-02-27 10:57:41 -0800

    whoops, made some mistakes with get-status last night

--- api/controllers/get-status.js
+++ api/controllers/get-status.js
@@ -2,10 +2,10 @@ module.exports = function (io, deploymentsStatus) {
     return function (req, res, next) {
         if (req.query.deployment) {
             var deployment = deploymentsStatus[req.query.deployment];
-            if (typeof deployment === 'object' && query !== null) {
-                req.status(200).json(deployment);
+            if (typeof deployment === 'object' && deployment !== null) {
+                res.status(200).json(deployment);
             } else {
-                req.status(404).json({
+                res.status(404).json({
                     status: 404,
                     msg: 'Sorry, we could not find the status of the deployment you are looking for.'
                 })
