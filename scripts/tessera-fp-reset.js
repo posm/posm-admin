6cc3c37407e3e3ab47f99308969718d3516037e7
Nicholas Hallahan <nick@theoutpost.io>
2016-03-04 11:57:46 -0800

    writing tessera conf files

--- scripts/tessera-fp-reset.js
+++ scripts/tessera-fp-reset.js
@@ -8,13 +8,16 @@ var TESSERA_CONF_DIR = '/etc/tessera.conf.d';
 
 if (typeof argv === 'object') {
     var manifestPath = argv._[0] || argv.m || argv.manifest;
-    tesseraFieldPapersReset(manifestPath);
+    if (typeof manifestPath === 'string') {
+        tesseraFieldPapersReset(manifestPath);
+    }
 }
 
 module.exports = tesseraFieldPapersReset;
 
 function tesseraFieldPapersReset(manifestPath, cb) {
-    fs.readFile(manifestPath, function (err, manifest) {
+    fs.readFile(manifestPath, 'utf-8', function (err, data) {
+        var manifest = JSON.parse(data);
         if (err) {
             var errObj = {
                 err: err,
@@ -36,12 +39,14 @@ function tesseraFieldPapersReset(manifestPath, cb) {
             } else {
                 console.error(errObj);
             }
+            return;
         }
 
         // Read Manifest
         var contents = manifest.contents;
         for (var filePath in contents) {
             var properties = contents[filePath];
+            if (typeof properties !== 'object' && typeof properties.type !== 'string') continue;
             if (properties.type.toLowerCase().indexOf('mbtiles') > -1) {
 
                 // handle MBTiles
@@ -50,7 +55,7 @@ function tesseraFieldPapersReset(manifestPath, cb) {
             } else if (properties.type.toLowerCase().indexOf('mapnik/xml') > -1) {
 
                 // handle Mapnik XML
-                writeTesseraConfig(manifest, manifestPath, filePath, 'mapnik');
+                writeTesseraConf(manifest, manifestPath, filePath, 'mapnik');
 
             }
         }
@@ -61,18 +66,28 @@ function tesseraFieldPapersReset(manifestPath, cb) {
         var fileName = path.parse(filePath).base.split('.')[0];
         var k = 'deployment/' + manifest.name + '/tiles/' + fileName;
         var v = protocol + '://' + path.parse(manifestPath).dir + '/' + filePath;
-        var conf =  {k: v};
+        var conf = {};
+        conf[k] = v;
+        var confJSON =  JSON.stringify(conf);
         var confPath = TESSERA_CONF_DIR + '/' + fileName + '.json';
-        fs.writeFile(confPath, conf, function(err) {
-            var errObj = {
-                err: err,
-                msg: 'There was a problem writing a tessera conf file.'
-            };
-            if (typeof cb === 'function') {
-                cb(errObj);
-            } else {
-                console.error(errObj);
+        fs.writeFile(confPath, confJSON, function(err) {
+            if (err) {
+                var errObj = {
+                    err: err,
+                    msg: 'There was a problem writing a tessera conf file.'
+                };
+                if (typeof cb === 'function') {
+                    cb(errObj);
+                } else {
+                    console.error(errObj);
+                }
+                return;
             }
+
         });
+    }
+
+    function writeFieldPapersConf() {
+
     }
 }
