1c6453969f9542e614fe8d988452a0e80dc8dece
Nicholas Hallahan <nick@theoutpost.io>
2016-03-03 17:10:52 -0800

    formatting

--- scripts/render-db-pbf2render.sh
+++ scripts/render-db-pbf2render.sh
@@ -9,15 +9,15 @@ newest_dump_path=$api_db_dumps_dir$newest_dump
 mem=$(awk 'NR == 1 { print int($2*.9/1024) } ' /proc/meminfo)
 
 osm2pgsql \
---create \
---hstore-all \
---hstore-add-index \
---extra-attributes \
---slim \
---drop \
---unlogged \
---database='gis' \
--C $mem \
---number-processes $(nproc) $newest_dump_path
+    --create \
+    --hstore-all \
+    --hstore-add-index \
+    --extra-attributes \
+    --slim \
+    --drop \
+    --unlogged \
+    --database='gis' \
+    -C $mem \
+    --number-processes $(nproc) $newest_dump_path
 
 echo "EXECUTED: render-db-pbf2render.sh"
