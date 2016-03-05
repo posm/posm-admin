0f0374dddc7820c3a1ea7636f50eb8d223003398
Nicholas Hallahan <nick@theoutpost.io>
2016-03-04 16:40:22 -0800

    making field papers conf file. need to open up permissions though

--- scripts/tessera-fp-reset.js
+++ scripts/tessera-fp-reset.js
@@ -5,6 +5,10 @@ var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
 
 var TESSERA_CONF_DIR = '/etc/tessera.conf.d';
+var FP_CONF_PATH = '/opt/fp/fp-web/config/providers.json';
+
+// FIXME This should not be hardcoded.
+var POSM_BASE_URL='http://posm.local';
 
 if (typeof argv === 'object') {
     var manifestPath = argv._[0] || argv.m || argv.manifest;
@@ -15,7 +19,13 @@ if (typeof argv === 'object') {
 
 module.exports = tesseraFieldPapersReset;
 
+//tesseraFieldPapersReset('/opt/data/deployments/dvizarasekwa/manifest.json');
+
 function tesseraFieldPapersReset(manifestPath, cb) {
+
+    // TODO Read current conf from file first.
+    var fpConf = {};
+
     fs.readFile(manifestPath, 'utf-8', function (err, data) {
         var manifest = JSON.parse(data);
         if (err) {
@@ -51,11 +61,13 @@ function tesseraFieldPapersReset(manifestPath, cb) {
 
                 // handle MBTiles
                 writeTesseraConf(manifest, manifestPath, filePath, 'mbtiles');
+                writeFieldPapersConf(manifest, filePath);
 
             } else if (properties.type.toLowerCase().indexOf('mapnik/xml') > -1) {
 
                 // handle Mapnik XML
-                writeTesseraConf(manifest, manifestPath, filePath, 'mapnik');
+                //writeTesseraConf(manifest, manifestPath, filePath, 'mapnik');
+                //writeFieldPapersConf(manifest, filePath);
 
             }
         }
@@ -64,7 +76,7 @@ function tesseraFieldPapersReset(manifestPath, cb) {
 
     function writeTesseraConf(manifest, manifestPath, filePath, protocol) {
         var fileName = path.parse(filePath).base.split('.')[0];
-        var k = 'deployment/' + manifest.name + '/tiles/' + fileName;
+        var k = 'tiles/' + manifest.name + '/' + fileName;
         var v = protocol + '://' + path.parse(manifestPath).dir + '/' + filePath;
         var conf = {};
         conf[k] = v;
@@ -81,13 +93,31 @@ function tesseraFieldPapersReset(manifestPath, cb) {
                 } else {
                     console.error(errObj);
                 }
-                return;
             }
-
         });
     }
 
-    function writeFieldPapersConf() {
+    function writeFieldPapersConf(manifest, filePath) {
+        var tileSet = manifest.contents[filePath];
+        var name = path.parse(filePath).base.split('.')[0];
+        var fpTileSet = fpConf[name] = {};
+        tileSet.label ? fpTileSet.label = tileSet.label : fpTileSet.label = name;
+        fpTileSet.template = POSM_BASE_URL + '/tiles/' + manifest.name + '/' + name + '/{z}/{x}/{y}.png';
+        fpTileSet.minzoom = tileSet.minzoom || 0;
+        fpTileSet.maxzoom = tileSet.maxzoom || 25;
 
+        fs.writeFile(FP_CONF_PATH, JSON.stringify(fpConf), function(err) {
+            if (err) {
+                var errObj = {
+                    err: err,
+                    msg: 'There was a problem writing the field papers conf file.'
+                };
+                if (typeof cb === 'function') {
+                    cb(errObj);
+                } else {
+                    console.error(errObj);
+                }
+            }
+        });
     }
 }
