a4752051a9b0cc7c7055e7e4b1c1a433b3ddc32b
Nicholas Hallahan <nick@theoutpost.io>
2016-03-06 11:50:01 -0800

    happy with omk-osm.sh script

--- scripts/omk-mbtiles.sh
+++ scripts/omk-mbtiles.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+# $1 - the path of the deployment
+deployment_path=$1
+manifest_path=$1/manifest.json
+derivatives_path=$deployment_path/derivatives
+mkdir -p $derivatives_path
+deployment_name=$(cat $manifest_path | jq -r '.name')
+
+bbox_left=$(cat $manifest_path | jq '.bbox[0]')
+bbox_bottom=$(cat $manifest_path | jq '.bbox[1]')
+bbox_right=$(cat $manifest_path | jq '.bbox[2]')
+bbox_top=$(cat $manifest_path | jq '.bbox[3]')
+
--- scripts/omk-osm.sh
+++ scripts/omk-osm.sh
@@ -1,1 +1,43 @@
 #!/usr/bin/env bash
+
+# $1 - the path of the deployment
+deployment_path=$1
+manifest_path=$1/manifest.json
+derivatives_path=$deployment_path/derivatives
+mkdir -p $derivatives_path
+deployment_name=$(cat $manifest_path | jq -r '.name')
+
+convert_pbf_to_xml() {
+    pbf=$deployment_path/contents/$1
+    derivatives=$deployment_path/derivatives
+    buildings_xml=$derivatives/$deployment_name-buildings.osm
+    pois_xml=$derivatives/$deployment_name-pois.osm
+
+    # Buildings
+    echo ''
+    echo '==> omk-osm.sh: Creating buildings OSM XML for OpenMapKit using Osmosis.'
+    echo ''
+    osmosis --read-pbf $pbf \
+        --tf accept-ways building=* \
+        --used-node \
+        --write-xml $buildings_xml
+
+    # POIs
+    echo ''
+    echo '==> omk-osm.sh: Creating POIs OSM XML for OpenMapKit using Osmosis.'
+    echo ''
+    osmosis --read-pbf $pbf \
+        --tf accept-ways building=* \
+        --used-node \
+        --write-xml $buildings_xml
+
+}
+
+contents_keys=$(cat $manifest_path | jq -r '.contents | keys | .[]')
+for key in $contents_keys
+do
+    type=$(cat $manifest_path | jq -r '.contents | .["'$key'"].type')
+    if [ $type = 'OSM/PBF' ]; then
+        convert_pbf_to_xml $key
+    fi
+done
