f6528cb66d30f8ad8e0fed73b8277ca3a6f2dbc9
Seth Fitzsimmons <seth@mojodna.net>
2016-03-09 01:04:21 -0500

    Interpolate variables properly

--- scripts/osm_api-db-populate.sh
+++ scripts/osm_api-db-populate.sh
@@ -33,13 +33,13 @@ echo "==> api-db-populate.sh: Sequences set."
 # re-create a ClientApplication entry for iD
 echo "==> creating credentials for iD"
 posm_base_url=$(jq -r .posm_base_url /etc/posm.json)
-osm_id_key=$(cd '/opt/osm/osm-web' && bundle exec rake osm:apps:create name='OSM iD' url='${posm_base_url}' | jq -r .key)
+osm_id_key=$(cd '/opt/osm/osm-web' && bundle exec rake osm:apps:create name='OSM iD' url="${posm_base_url}" | jq -r .key)
 
 # re-create the POSM user
 echo "==> creating a POSM account"
 osm_posm_user=$(jq -r .osm_posm_user /etc/posm.json)
 osm_posm_description=$(jq -r .osm_posm_description /etc/posm.json)
-cd '/opt/osm/osm-web' && bundle exec rake osm:users:create display_name='${osm_posm_user}' description='${osm_posm_description}'
+cd '/opt/osm/osm-web' && bundle exec rake osm:users:create display_name="${osm_posm_user}" description="${osm_posm_description}"
 
 # change the OSM iD key
 echo "==> changing OSM iD key as root"
