a1900f30af1733c5b759fe94edae717b0e9980a8
Nicholas Hallahan <nick@theoutpost.io>
2016-05-09 14:46:57 -0700

    #3 Timestamps are figured out by alphabetical order of script. render-db-update.sh which is run by the cron job should be creating something that is not sorted to be higher than the timestamped scripts in alphabetical order.

--- scripts/render-db-update.sh
+++ scripts/render-db-update.sh
@@ -5,10 +5,10 @@ scripts_dir=/opt/admin/posm-admin/scripts/
 echo "==> render-db-update.sh: Dumping API DB and updating Render DB (used by cron)."
 
 # sudo -u osm /opt/admin/posm-admin/scripts/osm_render-db-api2pbf.sh render-db-update-dump.pbf
-sudo -u osm $scripts_dir/osm_render-db-api2pbf.sh render-db-update-dump.pbf
+sudo -u osm $scripts_dir/osm_render-db-api2pbf.sh 0_render-db-update-dump.pbf
 
 # sudo -u gis /opt/admin/posm-admin/scripts/gis_render-db-pbf2render.sh render-db-update-dump.pbf
-sudo -u gis $scripts_dir/gis_render-db-pbf2render.sh render-db-update-dump.pbf
+sudo -u gis $scripts_dir/gis_render-db-pbf2render.sh 0_render-db-update-dump.pbf
 
 echo "==> render-db-update.sh: Restarting tessera."
 sudo service tessera restart
