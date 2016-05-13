914063e1c2d9e24c64ca88679581e9db5c6c5c4a
Daniel Baah <dbaah@spatialdev.com>
2016-05-13 16:22:49 -0700

    create fp backup script

--- scripts/backup-data.sh
+++ scripts/backup-data.sh
@@ -2,5 +2,8 @@
 
 scripts_dir=/opt/admin/posm-admin/scripts
 
-#back up api-db
+#back up osm api database
 sudo -u postgres $scripts_dir/osm_api-db-backup.sh /opt/data/api-db-dumps/
+
+#back up fieldpapers production database
+$scripts_dir/root_fp-production-db-backup.sh /opt/data/fp-db-dumps/
---
+++ scripts/root_fp-production-db-backup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# example
+# mysqldump -uroot -pposm fieldpapers_production | gzip > /opt/data/api-db-dumps/fieldpapers_production.sql.gz
+
+echo '=> root_fp-production-db-backup.sh'
+
+backup_path=$1
+
+echo '=> Backing up osm api database to: ' $backup_path
+echo '=> Dumping database...'
+
+#dump db - date format: YYYYMMDD-HH-MM/SS
+mysqldump -uroot -pposm fieldpapers_production | gzip > $backup_path/fp_production`date +%Y%m%d-%H%M:%S`
+
+echo '=> Complete...'
