fbbd1fafb900059e8b37af4bfc6767eaf23abe85
Daniel Baah <dbaah@spatialdev.com>
2016-05-17 11:49:17 -0700

    clean up echo in api db sh script

--- scripts/postgres_api-db-backup.sh
+++ scripts/postgres_api-db-backup.sh
@@ -3,7 +3,7 @@
 # example
 # sudo -u postgres /opt/admin/posm-admin/scripts/osm_api-db-backup.sh /opt/data/api-db-dumps/
 
-echo '=> postgres_api-db-backup.sh'
+echo '==> postgres_api-db-backup.sh'
 
 backup_path=$1
 
@@ -12,4 +12,4 @@ echo '=> Dumping database...'
 #dump db - date format: YYYYMMDD-HH-MM/SS
 pg_dump osm | gzip > $backup_path/osm`date +%Y%m%d-%H%M:%S`.sql.gz
 
-echo '=> Complete...'
+echo "==> postgres_api-db-backup.sh END"
