19e3a1fbaf83ba07a0e0d70c88897bb32a22db9e
Daniel Baah <dbaah@spatialdev.com>
2016-05-18 15:36:22 -0700

    move all backups to /opt/data/backups

--- scripts/backup-data.sh
+++ scripts/backup-data.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 scripts_dir=/opt/admin/posm-admin/scripts
+backups_dir=/opt/data/backups
 timestamp=`date +%Y%m%d-%H%M:%S`
 
 # back up osm api database
-sudo -u postgres $scripts_dir/postgres_api-db-backup.sh /opt/data/osm
+sudo -u postgres $scripts_dir/postgres_api-db-backup.sh $backups_dir/osm
 
 # back up field papers production database, atlas & snapshots
-$scripts_dir/root_fp-production-db-backup.sh /opt/data/fieldpapers
+$scripts_dir/root_fp-production-db-backup.sh $backups_dir/fieldpapers
 
 # back up omk forms and submissions
-mkdir /opt/data/omk/$timestamp
+# mkdir /opt/data/omk/$timestamp
 
-cp -a /opt/omk/OpenMapKitServer/data/. /opt/data/omk/$timestamp/
+# zip up omk data & save to backup directory
+tar -zcvf $backups_dir/omk/$timestamp.tar.gz /opt/omk/OpenMapKitServer/data/
--- scripts/root_fp-production-db-backup.sh
+++ scripts/root_fp-production-db-backup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # example
-# mysqldump -uroot -pposm fieldpapers_production | gzip > /opt/data/api-db-dumps/fieldpapers_production.sql.gz
+# /opt/admin/posm-admin/scripts/root_fp-production-db-backup.sh /opt/data/backups/fieldpapers
 
 timestamp=`date +%Y%m%d-%H%M:%S`
+backup_path=/opt/data/backups
 
 echo '=> root_fp-production-db-backup.sh'
 
@@ -11,14 +12,14 @@ backup_path=$1
 
 echo '=> Backing up osm api database to: ' $backup_path/$timestamp
 # create timestamp directory
-mkdir /opt/data/fieldpapers/$timestamp
+mkdir $backup_path/fieldpapers/$timestamp
 
 echo '=> Dumping database...'
 #dump mysql db - date format: YYYYMMDD-HH-MM/SS
 mysqldump -uroot -pposm fieldpapers_production | gzip > $backup_path/$timestamp/fp_prod_db.sql.gz
 
 echo '=> Copying field paper atlases & snapshots ...'
-# copy fp data into /opt/data/fieldpapers/
-cp -a /opt/fp/data/. /opt/data/fieldpapers/$timestamp/
+# copy fp data into $backup_path/fieldpapers/
+cp -a /opt/fp/data/. $backup_path/fieldpapers/$timestamp/
 
 echo '=> Complete...'
