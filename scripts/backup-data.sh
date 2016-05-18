c1817ffbd115584687d28623920431a819ed8159
Daniel Baah <dbaah@spatialdev.com>
2016-05-18 14:18:57 -0700

    create timestamp folder as gateway to related collection of backups

--- scripts/backup-data.sh
+++ scripts/backup-data.sh
@@ -4,12 +4,10 @@ scripts_dir=/opt/admin/posm-admin/scripts
 timestamp=`date +%Y%m%d-%H%M:%S`
 
 # back up osm api database
-sudo -u postgres $scripts_dir/postgres_api-db-backup.sh /opt/data/osm/
+sudo -u postgres $scripts_dir/postgres_api-db-backup.sh /opt/data/osm
 
-# back up field papers production database
-$scripts_dir/root_fp-production-db-backup.sh /opt/data/fieldpapers/
-
-#back up field papers atlases & snapshots
+# back up field papers production database, atlas & snapshots
+$scripts_dir/root_fp-production-db-backup.sh /opt/data/fieldpapers
 
 # back up omk forms and submissions
 mkdir /opt/data/omk/$timestamp
--- scripts/root_fp-production-db-backup.sh
+++ scripts/root_fp-production-db-backup.sh
@@ -3,14 +3,22 @@
 # example
 # mysqldump -uroot -pposm fieldpapers_production | gzip > /opt/data/api-db-dumps/fieldpapers_production.sql.gz
 
+timestamp=`date +%Y%m%d-%H%M:%S`
+
 echo '=> root_fp-production-db-backup.sh'
 
 backup_path=$1
 
-echo '=> Backing up osm api database to: ' $backup_path
-echo '=> Dumping database...'
+echo '=> Backing up osm api database to: ' $backup_path/$timestamp
+# create timestamp directory
+mkdir /opt/data/fieldpapers/$timestamp
 
+echo '=> Dumping database...'
 #dump mysql db - date format: YYYYMMDD-HH-MM/SS
-mysqldump -uroot -pposm fieldpapers_production | gzip > $backup_path/fp_production`date +%Y%m%d-%H%M:%S`
+mysqldump -uroot -pposm fieldpapers_production | gzip > $backup_path/$timestamp/fp_prod_db.sql.gz
+
+echo '=> Copying field paper atlases & snapshots ...'
+# copy fp data into /opt/data/fieldpapers/
+cp -a /opt/fp/data/. /opt/data/fieldpapers/$timestamp/
 
 echo '=> Complete...'
