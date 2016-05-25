c63b4ced74a8b1ec968d3d17c0932423b79f8196
Nicholas Hallahan <nick@theoutpost.io>
2016-05-25 14:05:11 -0700

    fix sed expression to work with or without quotes and take 3 parameters for z x y https://github.com/AmericanRedCross/posm/issues/109

--- scripts/root_change-fp-center.sh
+++ scripts/root_change-fp-center.sh
@@ -8,7 +8,8 @@ if [ $(whoami) != "root" ]; then
   exit 1
 fi
 
-sed -ri "s/(env DEFAULT_CENTER=)\".+\"$/\1\"${1}\"/" /etc/init/fp-web.conf
+# sudo /opt/admin/posm-admin/scripts/root_change-fp-center.sh 2 0.01 20.01
+sed -ri "s/(env DEFAULT_CENTER=)\"?.+\"?$/\1\"${1}\/${2}\/${3}\"/" /etc/init/fp-web.conf
 
 # We don't need to reset the fp-web service quite yet, because we will be
 # restarting it anyway in the conclusion of tessera-fp-reset.js
