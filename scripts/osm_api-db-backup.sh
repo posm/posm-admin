64dcd431465dc34c8f9523fd95a97c5ba150bda8
Seth Fitzsimmons <seth@mojodna.net>
2017-06-12 14:33:35 -0700

    Copy imported AOIs

--- scripts/osm_api-db-backup.sh
+++ scripts/osm_api-db-backup.sh
@@ -20,6 +20,9 @@ output="apidb-$(date +%Y%m%d%H%M%S).sql.gz"
 pg_dump osm | gzip > "${backup_path}/${output}"
 
 # link PBF dumps
-cp -alf /opt/data/api-db-dumps/* ${backup_path}
+cp -alf /opt/data/api-db-dumps/* $backup_path
+
+# copy AOIs
+cp -alf /opt/data/aoi/* $backup_path
 
 echo "==> $0 END"
