6efe3511f997c0b28813df3b178c15b8002bcd7c
Seth Fitzsimmons <seth@mojodna.net>
2017-06-12 13:38:50 -0700

    Use cp to generate hardlinks

--- scripts/osm_api-db-backup.sh
+++ scripts/osm_api-db-backup.sh
@@ -15,8 +15,6 @@ output="apidb-$(date +%Y%m%d%H%M%S).sql.gz"
 pg_dump osm | gzip > "${backup_path}/${output}"
 
 # link PBF dumps
-for dump in /opt/data/api-db-dumps/*; do
-  ln $dump $backup_path
-done
+cp -al /opt/data/api-db-dumps/ $backup_path
 
 echo "==> osm_api-db-backup.sh END"
