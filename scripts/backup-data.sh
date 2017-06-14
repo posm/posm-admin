bd51e84c7de47f6dd977679e710b854aef8b3869
Seth Fitzsimmons <seth@mojodna.net>
2017-06-14 13:00:30 -0700

    Deal with empty directories

--- scripts/backup-data.sh
+++ scripts/backup-data.sh
@@ -25,10 +25,10 @@ echo "==> Compressing omk data and backing up to $backups_dir/omk"
 sudo -u omk $scripts_dir/omk_backup.sh
 
 echo "==> Backing up imagery $backups_dir/imagery"
-cp -alf /opt/data/imagery/* ${backups_dir}/imagery
+cp -alf /opt/data/imagery/. ${backups_dir}/imagery
 
 echo "==> Backing up OpenDroneMap data to $backups_dir/opendronemap"
-cp -alf /opt/data/opendronemap/* ${backups_dir}/opendronemap
+cp -alf /opt/data/opendronemap/. ${backups_dir}/opendronemap
 
 echo "==> Backing up AOIs to $backups_dir/aoi"
-cp -alf /opt/data/aoi/* ${backups_dir}/aoi
+cp -alf /opt/data/aoi/. ${backups_dir}/aoi
